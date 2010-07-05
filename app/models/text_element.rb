fcf0be6f15da2494b429132113f7991636308af1
Steve England <steve@wearebeef.co.uk>
2010-07-05 15:33:51 +0100

    Remove dependancy on acts_as_textiled

--- app/models/text_element.rb
+++ app/models/text_element.rb
@@ -1,24 +1,24 @@
 class TextElement < ActiveRecord::Base
-  acts_as_textiled :value
-  
+  acts_as_textiled :value if respond_to?(:acts_as_textiled)
+
   validates_presence_of :var
-  
+
   #get with the variable as the called method
   def self.method_missing(method, *args)
     method_name = method.to_s
     super(method, *args)
-    
+
   rescue NoMethodError
     #retrieve a value
     te = object(method_name)
     "<div class=\"ugc\">#{te.value || dummy_text(te)}</div>"
   end
-  
+
   #retrieve the actual Setting record
   def self.object(var_name)
     TextElement.find_or_create_by_var(var_name.to_s)
   end
-  
+
   def self.dummy_text(te)
     "<p>#{te.var.titleize} dummy text. You can edit this text <a href=\"/admin/text_elements/#{te.id}\">here</a></p>"
   end
